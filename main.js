6530e253c50ca0ea773066daf5e57396902fd838
Palani <palanik987@gmail.com>
2022-12-29 21:32:05 +0530

    video-embed-update

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import {loadGLTF, loadVideo} from "../../libs/loader.js";
+import {loadGLTF, loadVideo} from "https://palanikjs.github.io/libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/sintel.mind',
+      imageTargetSrc: 'https://palanikjs.github.io/assets/targets/sintel.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
-    const video = await loadVideo("../../assets/videos/sintel/sintel.mp4");
+    const video = await loadVideo("https://palanikjs.github.io/assets/videos/sintel/sintel.mp4");
     const texture = new THREE.VideoTexture(video);
 
     const geometry = new THREE.PlaneGeometry(1, 204/480);
